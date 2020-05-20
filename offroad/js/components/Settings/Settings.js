4c21b702ac9fe4b743af7dc4133175cda03c9705
Harald Schafer <harald.the.engineer@gmail.com>
2020-05-19 17:17:02 -0700

    clean

--- offroad/js/components/Settings/Settings.js
+++ offroad/js/components/Settings/Settings.js
@@ -452,8 +452,8 @@ class Settings extends Component {
     }
 
     calib_description(params){
-      var intro = 'The calibration algorithm is always active and determines the orientation of the device relative to your car. ';
-      var end = 'Openpilot requires the up/down calibration to be within 5° and left/right calibration within 4°.';
+      var intro = 'The calibration algorithm is always active and determines the orientation of the device relative to your car.\n';
+      var end = 'openpilot requires the up/down calibration to be within 5° and left/right calibration within 4°.';
       var calib_json = JSON.parse(params);
       if (calib_json.hasOwnProperty('calib_radians')) {
         var calibArr = (calib_json.calib_radians).toString().split(',');
